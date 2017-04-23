bec178cc3656973c5159bc1fc838a9f270f05741
Bo-Xuan Fan <coffee0127@gmail.com>
2017-04-23 17:28:45 +0800

    透過 proxy 呼叫遠端 API

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  data: any;
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    this.data = this.http.get('/api/cars/find')
+      .map((response: Response) => response.json());
+  }
 }
