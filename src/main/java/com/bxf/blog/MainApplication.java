ae4bcecdbec04b3f5373d58364b1bdb1336d7a32
Bo-Xuan Fan <coffee0127@gmail.com>
2017-04-26 08:37:06 +0800

    Converting a Spring Boot JAR Application to a WAR

--- src/main/java/com/bxf/blog/MainApplication.java
+++ src/main/java/com/bxf/blog/MainApplication.java
@@ -25,6 +25,8 @@ package com.bxf.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * MainApplication
@@ -33,7 +35,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Bo-Xuan Fan
  */
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MainApplication.class);
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MainApplication.class, args);
